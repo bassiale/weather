07c03ba1c7bcf1ad2272be54bc64adb1c9c285fb
Skyhero-admin <smnouman007@gmail.com>
2021-08-16 21:19:41 +0530

    Tried adding beautifulsoup

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+s0#@h@6g5o4lwa1ed%d9)varr*ju7@na)(xl_s+*)249&!f$9'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
